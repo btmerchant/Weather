579836502e3daa7eff799d16cce7e70b234a9700
Brian Merchant <btmerchant@comcast.net>
2015-11-09 17:36:40 -0600

    Added nessisary code to enable OAuth - not using it yet

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,7 +8,8 @@ requirejs.config({
     "firebase" : "../lib/bower_components/firebase/firebase",
     "lodash" : "../lib/bower_components/lodash/lodash.min",
     // "material": "../lib/bower_components/bootstrap-material-design/dist/js/material.min",
-    'q': '../lib/bower_components/q/q'
+    'q': '../lib/bower_components/q/q',
+    "oauth": '../lib/bower_components/oauth-js/dist/oauth.min'
   },
   shim: {
     "bootstrap": ["jquery"],
@@ -19,8 +20,10 @@ requirejs.config({
 });
 
 require(
-  ["jquery", "lodash", "q", "bootstrap", "firebase", "dataCall", "dataWrite", "apiCall", "loginSignUp", "hbs!../templates/weather"],
-  function($, _, q, bootstrap, firebase, dataCall, dataWrite, apiCall, loginSignUp, weatherHBS) {
+  ["jquery", "lodash", "q", "bootstrap", "firebase", "oauth", "hbs", "dataCall", "dataWrite", "apiCall", "loginSignUp", "hbs!../templates/weather"],
+  function($, _, q, bootstrap, firebase, oauth, handlebars, dataCall, dataWrite, apiCall, loginSignUp, weatherHBS) {
+  // Working on getting OAuth to work
+  // OAuth.initialize('Lj4QOB73DKmntRgtPjY3_-rDiN0');
 
     //Sign Up view
   $(document).on("click", "#swLogIn", function() {
@@ -34,7 +37,7 @@ require(
   });
 
   zipCode = 37205;
-  $(document).on("click", "#submitZip", function() {
+  $(document).on("click", "#submit3", function() {
     zipCode =$("#zipCode").val();
    if (checkZip(zipCode)) {
       apiCall.getWeather(zipCode)
@@ -42,7 +45,23 @@ require(
           console.log('weather', weather);
           $("#zipSearchView").hide();
           $("#mainContentView").show();
-          $('#mainContent').append(weatherHBS({weather: weatherData}));
+          $('#mainContent').append(weatherHBS({weather3: weatherData}));
+        });
+    }
+    else {
+          alert('Please Enter A Valid Zipcode');
+    }
+  });
+
+  $(document).on("click", "#submit7", function() {
+    zipCode =$("#zipCode").val();
+   if (checkZip(zipCode)) {
+      apiCall.getWeather(zipCode)
+        .then(function(weather) {
+          console.log('weather', weather);
+          $("#zipSearchView").hide();
+          $("#mainContentView").show();
+          $('#mainContent').append(weatherHBS({weather7: weatherData}));
         });
     }
     else {
